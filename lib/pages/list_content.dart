13e61589ac625454f31c6ecee470e924d26d6758
franopx <franciscogalenorojas@gmail.com>
2025-04-27 18:05:36 -0400

    create list_content file

---
+++ lib/pages/list_content.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:logger/logger.dart';
+
+class ListContentPage extends StatelessWidget {
+  
+  
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
